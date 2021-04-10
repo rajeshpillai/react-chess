2665b4e7d19c80031d329b5407a6e796cc0c80cf
Rajesh Pillai <rajesh@algorisys.com>
2021-04-10 23:23:33 +0530

    Basic chess board

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function loadGameState() {
+  const data = [];
+  for(let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      data.push({i, j});
+    }
+  }
+
+  return data;
+}
+
+function App() {
+  const [gameState, setGameState] = 
+        useState(loadGameState());
+
+  const isEven = (num) => num % 2;
+
+  const buildBoard = () => {
+    const board = [];
+    for(let r = 0; r < 8; r++) {
+      for (let c = 0; c < 8; c++) {
+        const classname = (isEven(r) && isEven(c)) ||  (!isEven(r) && !isEven(c)) ? "white"  : "black";
+        board.push(
+          <div className={`cell ${classname}`}>
+            {r,c}
+          </div>
+        );
+  
+      }
+    }
+    return board;
+  } 
+
+  return (
+    <div className="container">
+      <div className="board">
+       {buildBoard()}    
+       </div>
+    </div>
+  );
+}
+
+export default App;
