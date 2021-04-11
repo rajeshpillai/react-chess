a785e9cd18ba2afcc407fc903aff6bfc2e8ef869
Rajesh Pillai <rajesh@algorisys.com>
2021-04-11 10:45:28 +0530

    Pieces design

--- src/App.js
+++ src/App.js
@@ -12,6 +12,36 @@ function loadGameState() {
   return data;
 }
 
+// Square component (container for pieces)
+function Square({type, meta, piece}) {
+  return (
+    <div className={`cell ${type}`}>
+        {meta.row},{meta.col}
+        {piece}
+    </div>
+  )
+}
+
+
+function Pawn({type}) {
+  return (
+    <div className="rook">
+      { type == 1 && <div className="black-pawn"></div> }
+      { type == 2 && <div className="white-pawn"></div> }
+    </div>
+  )
+}
+
+function Rook({type}) {
+  return (
+    <div className="rook">
+      { type == 1 && <div className="black-rook"></div> }
+      { type == 2 && <div className="white-rook"></div> }
+    </div>
+  )
+}
+
+
 function App() {
   const [gameState, setGameState] = 
         useState(loadGameState());
@@ -25,23 +55,65 @@ function App() {
       let start = 65;
       for (let c = 0; c < 8; c++) {
         const classname = (isEven(r) && isEven(c)) ||  (!isEven(r) && !isEven(c)) ? "white"  : "black";
-        board.push(
-          <div className={`cell ${classname}`}>
-            {r + 1} {String.fromCharCode(start++)}
-            { r == 1 && <div className="black-pawn"></div> }
-            { r == 6 && <div className="white-pawn"></div> }
-          </div>
-        );
-  
+        board.push({ 
+          row: r,
+          col: c,
+          type: classname,
+          piece: undefined,
+          cell: <Square type= {classname} meta={{row: r, col: c}} />
+
+        });
       }
     }
+    for(let i = 8; i < 16; i++) {
+      board[i].piece = <Pawn type="1" />
+      board[i + 40].piece = <Pawn type="2" />
+    }
+
+    board[0].piece = <Rook type="1" />
+    board[7].piece = <Rook type="1" />
+    board[56].piece = <Rook type="2" />
+    board[63].piece = <Rook type="2" />
+
     return board;
   } 
+
+  const setup = () => {
+    const board = buildBoard();
+    // Setup pawns
+    // for(let i = 8; i < 8 + 8; i++) {
+    //   board[i] = <Pawn type="1"/>
+    // }
+    return board;
+  }
+
+  const render = () => {
+    const board = setup();
+    console.log({board});
+    const pieces = [];
+    
+    board.forEach((square, i)  => {
+      //const loc = <Square type= {square.type} meta={square} piece={<Pawn type="1"/>} />;
+      // const loc = <Square type= {square.type} meta={square}  />;
+      pieces.push(
+        <Square type={square.type} meta={square} piece={square.piece} />
+     );
+      
+    });
+
+
+
+    //  pieces[8].piece = <Pawn type="1" />
+    // board[i + 40].piece = Pawn(1);
+    
+
+    return pieces;
+  }
 
   return (
     <div className="container">
       <div className="board">
-       {buildBoard()}    
+       {render()} 
        </div>
     </div>
   );
